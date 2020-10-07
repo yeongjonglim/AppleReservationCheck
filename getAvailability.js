644fa78defefc1b77342572e0f9bee25ffc9dfff
Yeong Jong Lim <yeongjong.lim@gmail.com>
2020-10-08 07:43:58 +0800

    Fixed date problem and message formatting

--- getAvailability.js
+++ getAvailability.js
@@ -3,7 +3,8 @@ const fetch = require('node-fetch')
 const _ = require('lodash')
 const TelegramBot = require('node-telegram-bot-api')
 const bot = new TelegramBot(process.env.TELEGRAM_BOT_API)
-const availabilityUrl = `https://retail-pz.cdn-apple.com/product-zone-prod/availability/${new Date().toISOString().substring(0, 10)}/15/availability.json`
+const currDate = new Date()
+const availabilityUrl = `https://retail-pz.cdn-apple.com/product-zone-prod/availability/${currDate.getUTCFullYear()}-${pad(currDate.getUTCMonth()+1)}-${pad(currDate.getUTCDate())}/${currDate.getUTCHours()}/availability.json`
 const locationUrl = 'https://www.apple.com/pza-irb/stores?stageRootPath=%2Fsg&lat=1.29&long=103.86'
 
 function getAvailability() {
@@ -13,7 +14,7 @@ function getAvailability() {
     fetch(availabilityUrl)
         .then(response => {
             if (response.status !== 200) {
-                console.log('Error!')
+                console.log('Failed getting availability!')
                 return
             }
             response.json().then(data => {
@@ -21,7 +22,7 @@ function getAvailability() {
                 fetch(locationUrl)
                     .then(response => {
                         if (response.status !== 200) {
-                            console.log('Error!')
+                            console.log('Failed getting location!')
                             return
                         }
                         response.json().then(stores => {
@@ -42,6 +43,10 @@ function getAvailability() {
         })
 }
 
+function pad(d) {
+    return (d < 10) ? '0' + d.toString() : d.toString();
+}
+
 function joinTwoArrayObjects(arr1, arr2) {
     return _.map(arr1, function(item){
         return _.extend(item, _.find(arr2, { storeNum: item.storeNumber }));
@@ -52,14 +57,17 @@ function sendTelegramMessage(avail) {
     avail.forEach(store => {
         let textMessage
         if (store.appointmentsAvailable) {
-            textMessage = `Appointment available at ${store.address.title} for the time ${new Date(store.firstAvailableAppointment).toString()}`
+            textMessage = `Appointment available at <b>${store.address.title}</b> for the time\n<b>${new Date(store.firstAvailableAppointment*1000).toString()}</b>.\n<a href="https://www.apple.com/sg/retail/instore-shopping-session/">Reserve Now!</a>`
         }
         else {
             textMessage = `Appointment not available at ${store.address.title}`
         }
         bot.sendMessage(
             process.env.CHAT_ID,
-            textMessage
+            textMessage,
+            {
+                parse_mode: 'HTML'
+            }
         )
     })
 }
